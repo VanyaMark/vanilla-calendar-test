726d4bbf611d150a0fc56b7197021cf66eb4c0e6
VanyaMark <markova.vanya@gmail.com>
2024-09-08 21:32:03 +0200

    working disabled dates

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
     const options = {
         settings: {
+            range: {
+                min: '2022-07-01',
+                max: '2022-09-30',
+                disabled: ['2022-08-10:2022-08-13', '2022-08-22'],
+            },
             selected: {
-                dates: ['2024-08-20'],
+                year: 2022,
+                month: 7,
             },
         },
     };
 
+    // Initialize Vanilla Calendar
     const calendar = new VanillaCalendar('#calendar', options);
     calendar.init();
 });
+// document.addEventListener('DOMContentLoaded', () => {
+//     const options = {
+//         settings: {
+//             selected: {
+//                 dates: ['2024-08-20'],
+//             },
+//         },
+//     };
+
+//     const calendar = new VanillaCalendar('#calendar', options);
+//     calendar.init();
+// });
+
+// import VanillaCalendar from 'vanilla-calendar-pro';
+// import { IOptions } from 'vanilla-calendar-pro/types';
+// import 'vanilla-calendar-pro/build/vanilla-calendar.min.css';
+
+// const options: IOptions = {
+//   settings: {
+//     range: {
+//       min: '2022-07-01',
+//       max: '2022-09-30',
+//       disabled: ['2022-08-10:2022-08-13', '2022-08-22'],
+//     },
+//     selected: {
+//       year: 2022,
+//       month: 7,
+//     },
+//   },
+// };
+
+// const calendar = new VanillaCalendar('#calendar', options);
+// calendar.init();
